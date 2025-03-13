35a424249149d87db2d5a56621cbc6b155ccd3e0
Chukwudi Chima <chchi@nhsbsa.nhs.uk>
2025-03-13 15:11:42 +0000

    pension credit price uplift 2025

---
+++ app/data/session-data-defaults.js
@@ -0,0 +1,13 @@
+//
+// DATA
+//
+
+module.exports = {
+    MinimumSingleGuaranteeCredit: '£227.10',
+
+    MinimumCoupleGuaranteeCredit: '£346.60',
+
+    MaximumSingleSavingsCredit: '£17.30',
+
+    MaximumCoupleSavingsCredit: '£19.36',
+}
